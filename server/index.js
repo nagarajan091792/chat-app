6b388e54e942ba7d29db41e44ea0c1676f52cfd8
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2023-05-03 11:12:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://zippy-druid-a90a1f.netlify.app",
+    origin: "https://deft-blini-e1a304.netlify.app/",
     credentials: true,
   },
 });
